1bd0bb923a8a249f36eb8c06857fd5d554d11451
Nite <solitude.loner7@gmail.com>
2020-12-25 23:30:27 +0700

    [Update] User can see a display panel where the key value and key code will be displayed when the key is pressed

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const app = (() => {
-    const _keyboardContainer = document.querySelector('.container__keyboard');
-    const _ouputContainer = document.querySelector('.container__output');
+    const _titleContainer = document.querySelector('.container__title');
+    const _outputContainer = document.querySelector('.container__output');
+    const _container = document.querySelector('.container');
+    const _charCodeDisplay = document.querySelector('.container__output__char-code');
+    const _keyContainer = document.querySelector('.container__output__desc__key');
+    const _codeContainer = document.querySelector('.container__output__desc__code');
+    const _keyDisplay = document.querySelector('.container__output__desc__key__content');
+    const _codeDisplay = document.querySelector('.container__output__desc__code__content');
 
-    function renderKeyboard() {
-        
+    document.body.addEventListener('keypress', (event) => {
+        hideTitleContainer();
+        showOutputContainer();
+
+        renderCharCodeDisplay(event.charCode);
+        renderKeyDisplay(event.key);
+        renderCodeDisplay(event.code);
+    })
+
+
+    function hideTitleContainer() {
+        _titleContainer.classList.add('hidden');
     }
 
-    function createKeyDOM() {
-
+    function showOutputContainer() {
+        _outputContainer.classList.remove('hidden');
     }
+
+    function renderCharCodeDisplay(charCode) {
+        _charCodeDisplay.textContent = charCode;
+    }
+
+    function renderKeyDisplay(key) {
+        _keyDisplay.textContent = key;
+    }
+
+    function renderCodeDisplay(code) {
+        _codeDisplay.textContent = code;
+    }
+    
 })();
